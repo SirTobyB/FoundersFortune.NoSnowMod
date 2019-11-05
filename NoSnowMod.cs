d64db8fff314f1af677d18b68bf52b0cfccb2cf2
Sir Toby <baldur_angbant@web.de>
2019-11-05 19:53:24 +0100

    add first version of the mod

---
+++ NoSnowMod.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+
+// It's good practice to keep all of your source code in your own namespace
+// Founders' Fortune doesn't use an own namespace though.
+namespace NoSnowMod
+{
+    // System.Serializable is obligatory.
+    [System.Serializable]
+    public class NoSnowMod : Mod 
+    {
+        // The Load() function is called when the mods are loaded into memory.
+        // This happens either before entering the colonist selection screen when you click on "New Game"
+        // or during the loading process when you load an old one
+        public override void Load() 
+        {
+            MonoBehaviour.print("loaded mod NoSnow");
+        }
+
+        // Start() is called after the game world has been loaded and initialized
+        // This function is called every time a world has been loaded, doesn't matter if it's a new game or loaded from a save game
+        public override void Start() 
+        {
+            MonoBehaviour.print("started mod NoSnow");
+        }
+
+        // Update() is called every single frame.
+        // Be very careful that you don't put anything too performance-heavy directly in here or you will easily slow down the player's game.
+        public override void Update() 
+        {
+            // Disable the moving of the snowflake objects, so that they become visible, completely
+            //MonoBehaviour.print("disable snow");
+            WorldScripts.Instance.weatherManager.precipitationManager.SetIsSnowing(false, false);
+        }
+    }
+}
+
